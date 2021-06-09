d3ea7395f10ea5543230f04b718b97619eba5a2f
SrD4rkOficial <antonio.martim.matos@gmail.com>
2021-06-09 11:59:09 +0100

    Update 10

--- src/PSI1L_M4567_2220077_AntonioMatos/main.c
+++ src/PSI1L_M4567_2220077_AntonioMatos/main.c
@@ -44,6 +44,12 @@ struct mysqlvalues
     char query9[200];
     char query10[200];
     char query11[200];
+    char query12[200];
+    char query13[200];
+    char query14[200];
+    char query15[200];
+    char query16[200];
+    char query17[200];
 };
 
 struct randomnum
@@ -333,18 +339,12 @@ transferencias:
                 printf("\nInsira a quantidade em euros que deseja enviar.\n");
                 printf("Quantidade: ");
                 scanf("%i", &transferenciadata.money);
-                printf("1\n");
                 if(transferenciadata.money < 1)
                 {
                     printf("A quantidade tem de ser acima de 1.");
                     sleep(3);
                     goto transferencias;
                 }
-                printf("1\n");
-                printf(transferenciadata.referencia);
-                printf("1\n");
-                printf("\n");
-                printf(transferenciadata.entidade);
                 sprintf(mysqlvalues.query9, "SELECT * FROM `contas` WHERE `Cartao_referencia` = '%i' AND `Cartao_entidade` = '%i';", transferenciadata.referencia, transferenciadata.entidade);
                 if(mysql_query(con, mysqlvalues.query9))
                 {
@@ -359,21 +359,91 @@ transferencias:
                     sleep(3);
                     goto transferencias;
                 }
-                printf("Here");
-                if(row[2] < transferenciadata.money)
+                mysql_free_result(res);
+                sprintf(mysqlvalues.query12, "SELECT * FROM `contas` WHERE `Username` = '%s';", login.username);
+                if(mysql_query(con, mysqlvalues.query12))
+                {
+                    printf("Query failed: %s\n", mysql_error(con));
+                    exit(0);
+                }
+                res = mysql_use_result(con);
+                row = mysql_fetch_row(res);
+                int money = atoi(row[2]);
+                if(transferenciadata.money > money)
                 {
                     printf("Não tem saldo suficiente para efetuar esta operação.\n");
                     sleep(3);
                     goto transferencias;
                 }
-                printf("Here");
-                sprintf(mysqlvalues.query10, "UPDATE `contas` SET Saldo = current+%i WHERE Cartao_entidade = '%s' AND Cartao_referencia = '%s';", transferenciadata.money, transferenciadata.entidade, transferenciadata.referencia);
+                mysql_free_result(res);
+                sprintf(mysqlvalues.query10, "UPDATE `contas` SET Saldo = Saldo + %i WHERE Cartao_entidade = '%i' AND Cartao_referencia = '%i';", transferenciadata.money, transferenciadata.entidade, transferenciadata.referencia);
                 if(mysql_query(con, mysqlvalues.query10))
                 {
                     printf("Query failed: %s\n", mysql_error(con));
                     exit(0);
                 }
-                printf("Dinheiro transferido com sucesso.");
+                sprintf(mysqlvalues.query13, "UPDATE `contas` SET Saldo = Saldo - %i WHERE Username = '%s';", transferenciadata.money, login.username);
+                if(mysql_query(con, mysqlvalues.query13))
+                {
+                    printf("Query failed: %s\n", mysql_error(con));
+                    exit(0);
+                }
+                printf("Dinheiro transferido com sucesso.\n");
+                pressanykey();
+                goto menu;
+                break;
+            case 2:
+                printf("Insira o IBan para qual quer enviar.\n");
+                printf("IBan: ");
+                gets(transferenciadata.iban);
+                printf("Insira a quantidade que deseja enviar.\n");
+                printf("Quantidade: ");
+                scanf("%i", &transferenciadata.money);
+                fflush(stdin);
+                sprintf(mysqlvalues.query14, "SELECT * FROM `contas` WHERE `IBan` = '%s';", transferenciadata.iban);
+                if(mysql_query(con, mysqlvalues.query14))
+                {
+                    printf("Query failed: %s\n", mysql_error(con));
+                    exit(0);
+                }
+                res = mysql_use_result(con);
+                row = mysql_fetch_row(res);
+                if(row == NULL)
+                {
+                    printf("O perfil inserido não foi encontrado. Tente novamente.\n");
+                    sleep(3);
+                    goto transferencias;
+                }
+                mysql_free_result(res);
+                sprintf(mysqlvalues.query15, "SELECT * FROM `contas` WHERE `Username` = '%s';", login.username);
+                if(mysql_query(con, mysqlvalues.query15))
+                {
+                    printf("Query failed: %s\n", mysql_error(con));
+                    exit(0);
+                }
+                res = mysql_use_result(con);
+                row = mysql_fetch_row(res);
+                money = atoi(row[2]);
+                if(transferenciadata.money > money)
+                {
+                    printf("Não tem saldo suficiente para efetuar esta operação.\n");
+                    sleep(3);
+                    goto transferencias;
+                }
+                mysql_free_result(res);
+                sprintf(mysqlvalues.query16, "UPDATE `contas` SET Saldo = Saldo + %i WHERE IBan = '%s';", transferenciadata.money, transferenciadata.iban);
+                if(mysql_query(con, mysqlvalues.query16))
+                {
+                    printf("Query failed: %s\n", mysql_error(con));
+                    exit(0);
+                }
+                sprintf(mysqlvalues.query17, "UPDATE `contas` SET Saldo = Saldo - %i WHERE Username = '%s';", transferenciadata.money, login.username);
+                if(mysql_query(con, mysqlvalues.query17))
+                {
+                    printf("Query failed: %s\n", mysql_error(con));
+                    exit(0);
+                }
+                printf("Dinheiro transferido com sucesso.\n");
                 pressanykey();
                 goto menu;
                 break;
