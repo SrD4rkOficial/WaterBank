a39e6cdaa91cc081adff0104e31045198e2dd930
SrD4rkOficial <antonio.martim.matos@gmail.com>
2021-06-09 12:05:05 +0100

    Update 11

--- src/PSI1L_M4567_2220077_AntonioMatos/main.c
+++ src/PSI1L_M4567_2220077_AntonioMatos/main.c
@@ -67,15 +67,6 @@ struct switches switches;
 
 ////////////////////////////////////////
 
-void finish_with_error(MYSQL *con)
-{
-    fprintf(stderr, "%s\n", mysql_error(con));
-    mysql_close(con);
-    exit(1);
-}
-
-////////////////////////////////////////
-
 int main()
 {
     FILE *ficheiro;
